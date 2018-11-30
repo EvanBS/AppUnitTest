b84d5dbdf7b2397d806aa89f17e94e77a4c8d02a
EvanBS <evandiesp@gmail.com>
2018-11-30 23:15:45 +0200

    Добавьте файлы проекта.

---
+++ AppUnitTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AppUnitTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
